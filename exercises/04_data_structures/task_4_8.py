035289e4eb911b3db835647aea7d1edbf85bf324
wz0r <44982754+wz0r@users.noreply.github.com>
2025-02-03 11:12:18 +0300

    Update task_4_8.py

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,14 @@
 """
 
 ip = "192.168.3.1"
+
+ip.split(sep='.')
+['192', '168', '3', '1']
+
+print(ip_template)
+
+Ip Address:
+{0:<8} {1:<8} {2:<8} {3:<8}
+{0:08b} {1:08b} {2:08b} {3:08b}
+
+print(ip_template.format(oct1,oct2,oct3,oct4))
